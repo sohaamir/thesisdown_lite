e1ca67d5d893feac2f339298857c154583846453
Aamir Sohail <axs2210@student.bham.ac.uk>
2024-05-30 18:36:16 +0100

    moved binder dependencies to root

--- binder/install.R
+++
@@ -1,2 +0,0 @@
-# install.R
-install.packages(c("bookdown", "tinytex", "dplyr", "ggplot2", "ggsignif", "gridExtra", "plan"), repos='https://cloud.r-project.org/')
---
+++ install.R
@@ -0,0 +1,3 @@
+# install.R
+install.packages(c("remotes", "bookdown", "tinytex", "dplyr", "ggplot2", "ggsignif", "gridExtra", "plan"), repos='https://cloud.r-project.org/')
+remotes::install_github('lcreteig/amsterdown')
